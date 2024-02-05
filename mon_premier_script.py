913e4f92ebb568a860b58276ade98e2413658787
Amaury Gillot <84983643+Amom0@users.noreply.github.com>
2024-02-05 14:07:39 +0000

    feat: Clean code exercice count names

--- mon_premier_script.py
+++ mon_premier_script.py
@@ -3,10 +3,11 @@ import unittest
 """
 Count names with more than seven letters
 """
-def names(prenoms):
+def Compte_prenom_longueur(prenoms: list[str]) -> str:
+    nombre_limite_lettres = 7
     more_than_seven = 0
     for prenom in prenoms:
-        if len(prenom) > 7:
+        if len(prenom) > nombre_limite_lettres:
             more_than_seven += 1
             print("Prenom supérieur à 7 : " + prenom)
         else:
@@ -14,9 +15,9 @@ def names(prenoms):
     return more_than_seven
 
 class TestNamesMethod(unittest.TestCase):
-     def test_names(self):
+     def test_Compte_prenom_longueur(self):
         prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-        more_than_seven = names(prenoms=prenoms)
+        more_than_seven = Compte_prenom_longueur(prenoms=prenoms)
         self.assertEqual(more_than_seven, 4)
 
 if __name__ == '__main__':
